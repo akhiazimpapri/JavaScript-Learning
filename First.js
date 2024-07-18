5fb456b7b4c1c332d65121676319ee1125c17a7a
Akhi Azim Papri <akhiazimru@gmail.com>
2024-07-18 12:30:42 +0600

    Variables

--- First.js
+++ First.js
@@ -1,1 +1,15 @@
 console.log("Akhi Azim Papri");
+
+fullName = "Tony Stark"
+age = 24;
+price = 99.99;
+x = null;
+y = undefined;
+isFollow = true; //boolian = true or false.
+
+console.log(fullName);
+console.log(age);
+console.log(price);
+console.log(x);
+console.log(y);
+console.log(isFollow);
