0172f983ab9367b8b5503871de7229ceb89b4541
Akhi Azim Papri <akhiazimru@gmail.com>
2024-07-19 23:11:36 +0600

    L3 Practice Qs1

--- Script.js
+++ Script.js
@@ -1,21 +1,7 @@
-let number = prompt("Enter the mark:");
-let grade;
-
-if(number<=100 && number>=90){
-    grade = "A";
+for(let i = 0; i <= 100; i++)
+{
+    if(i % 2 === 0)
+    {
+        console.log("i =",i);
+    }
 }
-else if(number<=89 && number>=70){
-    grade = "B";
-}
-else if(number<=69 && number>=60){
-    grade = "C";
-}
-
-else if(number<=59 && number>=50){
-    grade = "D";
-}
-else if(number<=49){
-    grade = "F";
-}
-
-console.log("The student's grade is",grade,".");
