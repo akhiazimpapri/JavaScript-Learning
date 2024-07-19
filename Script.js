b02c41a21db2611b01b6ecb9207ad30fe2e54345
Akhi Azim Papri <akhiazimru@gmail.com>
2024-07-20 01:05:10 +0600

    L4 Practice Qs1

--- Script.js
+++ Script.js
@@ -1,6 +1,11 @@
-let fullName;
- fullName = prompt("Enter your fullNam without space.");
+let marks = [85,97,44,37,76,60];
+let sum = 0;
+let length = marks.length;
+for(let i = 0; i<length; i++)
+{
+    sum = sum + marks[i];
+}
 
- let userName = "@" + fullName + fullName.length;
+let avg = sum / length;
 
- console.log(userName);
+console.log(`Avg marks of the class = ${avg}`);
