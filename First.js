a022039e9d2a8d4775ce1ffba681a76d607d3b9b
Akhi Azim Papri <akhiazimru@gmail.com>
2024-07-19 11:49:57 +0600

    practice Qs1

--- First.js
+++ First.js
@@ -1,17 +1,8 @@
-const student = {
-    fullName : "Akhi Azim Papri",
-    age : 22,
-    cgpa : 3.03,
-    isPass : true,
-    session : "2020-21",
+const product = {
+    title: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price: 270,
 };
 
-student["fullName"] = "Pakhi";
-student["age"] = student["age"] + 1; //increasing value
-
-console.log(student["age"]);  
-// or 
-console.log(student.cgpa);
-console.log(student.fullName);
-
-//const variable cannot be changed. But a const object's keys can be changed.
+console.log(product);
