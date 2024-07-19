42d901fed4c4e2390ee2ef427c6a2e3cc0966652
Akhi Azim Papri <akhiazimru@gmail.com>
2024-07-19 18:11:06 +0600

    L2 Practice Qs2

--- Script.js
+++ Script.js
@@ -1,8 +1,21 @@
-let number = prompt("Enter a number:");
+let number = prompt("Enter the mark:");
+let grade;
 
-if(number % 5 === 0){
-    console.log(number, "is a multiple of 5.");
+if(number<=100 && number>=90){
+    grade = "A";
 }
-else{
-    console.log(number, "is NOT a multiple of 5.");
+else if(number<=89 && number>=70){
+    grade = "B";
 }
+else if(number<=69 && number>=60){
+    grade = "C";
+}
+
+else if(number<=59 && number>=50){
+    grade = "D";
+}
+else if(number<=49){
+    grade = "F";
+}
+
+console.log("The student's grade is",grade,".");
