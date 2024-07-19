a1766e762e1979d6b9da5ccc8c7079e81bb61337
Akhi Azim Papri <akhiazimru@gmail.com>
2024-07-19 16:04:40 +0600

    Arithmatic Operators

---
+++ Script.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 2;
+let c = a + b;
+console.log("a + b =", c);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a ** b =", a ** b); //Exponentiation, a^b.
+console.log("a++ =", a++);
+console.log("++a =", ++a);
+console.log("a-- =", a--);
+console.log("--a =", --a);
