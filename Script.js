61ab3d4afff57ba876ff31d63ef282f1b1e67573
Akhi Azim Papri <akhiazimru@gmail.com>
2024-07-20 15:54:34 +0600

    L5 function

--- Script.js
+++ Script.js
@@ -1,7 +1,22 @@
-let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+function myFuncton() {
+    console.log("Akhi Azim Papri");
+    console.log("I am learning JS");
+}
+myFuncton();
 
-let ret = companies.shift();
+function returnFunction(number, n) {
+    console.log(number);
+    console.log(number * n);
+    sum = number + n;
+    return sum; 
+}
+returnFunction(50, 3);
+let re = returnFunction(50, 3);
+console.log(re);
 
-companies.splice(2,1,"Ola");
-
-companies.push("Amazon");
+//arrow function
+const sumi = (a,b) => {
+    return a + b;
+} 
+console.log(sumi);
+console.log(sumi(3,4));
