0c06d5b44f8474777178d53256f8a318febfe424
Akhi Azim Papri <akhiazimru@gmail.com>
2024-07-20 11:22:11 +0600

    L4 Practice Qs3

--- Script.js
+++ Script.js
@@ -1,11 +1,7 @@
-let prices = [250,645,300,900,50];
-let length = prices.length;
+let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
 
-let i = 0;
-for(let price of prices)
-{
-    console.log(`value at index ${i} = ${price}`);
-    price = price - (price / 10);
-    console.log(`value after offer = ${price}`);
-    i++;
-}
+let ret = companies.shift();
+
+companies.splice(2,1,"Ola");
+
+companies.push("Amazon");
