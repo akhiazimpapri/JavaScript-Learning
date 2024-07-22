2a3a24eb57ad2244ef726989785994d494f384b3
Akhi Azim Papri <akhiazimru@gmail.com>
2024-07-22 22:58:45 +0600

    L5 practice Qs3

--- Script.js
+++ Script.js
@@ -1,13 +1,5 @@
-let string = prompt("Enter a string of lowerCase.")
-let findVowels = (str) => {
-    let count = 0;
-    for(const char of str)
-    {
-        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u")
-        {
-            count++;
-        }
-    }
-    return count;
-}
-console.log(findVowels(string));
+let arr = [1,2,3,4,5,6,7,8,9];
+
+arr.forEach((val) =>{
+    console.log(val * val); //val ** 2
+});
